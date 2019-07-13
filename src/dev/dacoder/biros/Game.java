d7ed74fc6e6f61fa8cbfff34530f91d453e719ca
Gabriel T Marques <gabrieltmm@gmail.com>
2019-07-13 09:32:08 -0300

    Changed Main class to Game class
    Signed-off-by: Gabriel T Marques <gabrieltmm@gmail.com>

--- src/dev/dacoder/biros/Game.java
+++ src/dev/dacoder/biros/Game.java
@@ -3,16 +3,16 @@ package dev.dacoder.biros;
 import dev.dacoder.biros.gui.Window;
 import dev.dacoder.biros.logic.GameLogic;
 
-public class Main {
+public class Game {
 
   public static void main(String[] args) {
-    System.out.println("[Main]: Starting...");
+    System.out.println("[Game]: Starting...");
     final String GAME_WINDOW_TITLE = "Biro's Adventure";
 
     Window.createWindow(GAME_WINDOW_TITLE);
     Window.setVisible();
     GameLogic.initialize();
 
-    System.out.println("[Main]: Started!");
+    System.out.println("[Game]: Started!");
   }
 }
